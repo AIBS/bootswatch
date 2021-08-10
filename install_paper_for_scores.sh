a7c05a9e2dbb2ce00d4d0a074e0c92ecd194d392
Joel Wagener <jwagener@aibs.org>
2021-08-09 18:55:57 -0600

    Adding changes to v3_aibs branch, which allows custom compilation of Bootswatch 3 Paper theme via paper/bootswatch.less and paper/variables.less.  Compiled paper/bootstrap.css file can be moved to SCORES application repositories via configuring install_paper_for_scores.sh. This commit includes needed changes for various accessibility compliances in SCORES Contracts, and is expected to change further to accommodate Reviews and Projects. JW

---
+++ install_paper_for_scores.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Use this script to move the compiled bootstrap.css file into the SCORES applications, excluding Solicitations.
+#
+# To install Bootswatch for making the required local changes...
+# 1. brew install npm # https://bootswatch.com/3/help/
+# 2. npm install -g grunt-cli # https://gruntjs.com/getting-started#installing-the-cli
+# 3. npm install # From within your bootswatch repository
+# 4. grunt swatch:paper # The theme we use is paper
+#
+# You can use these commands to verify file changes...
+# diff variables.less ../../bootswatch_170122/paper/variables.less # Example
+# sha1sum ./paper/bootstrap.css # Example, gives file hash
+#
+# IMPORTANT
+# To compare changes made here for future upgrades, diff the variables.less and bootswatch.less files here with originals from https://bootswatch.com/3/
+
+timestamp() {
+  date +"%y%m%d-%H%M%S";
+}
+
+# Update this array to control which applications are updated
+apps_to_update=("scores_contracts") # "scores_projects" "scores_reviews")
+
+for app in "${apps_to_update[@]}"; do
+  mv "../$app/app/assets/stylesheets/bootstrap.css.scss" "../$app/app/assets/stylesheets/bootstrap.css.scss-$(timestamp)"
+  cp "./paper/bootstrap.css" "../$app/app/assets/stylesheets/bootstrap.css.scss"
+  echo "$app bootstrap.css.scss backed up, current build installed";
+done
