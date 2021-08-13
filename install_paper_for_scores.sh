853bb38f767d681dc8a516f162d339431ada0114
Joel Wagener <jwagener@aibs.org>
2021-08-13 11:32:08 -0600

    Updated custom shared paper Bootstrap theme. JW

--- install_paper_for_scores.sh
+++ install_paper_for_scores.sh
@@ -20,8 +20,8 @@ timestamp() {
 }
 
 # Update this array to control which applications are updated
-# apps_to_update=("scores_contracts" "scores_projects" "scores_reviews") # All applications
-apps_to_update=("scores_reviews" "scores_contracts") # N applications
+apps_to_update=("scores_contracts" "scores_projects" "scores_reviews") # All applications
+# apps_to_update=("scores_projects") # N applications
 
 for app in "${apps_to_update[@]}"; do
   mv "../$app/app/assets/stylesheets/bootstrap.css.scss" "../$app/app/assets/stylesheets/bootstrap.css.scss-$(timestamp)"
